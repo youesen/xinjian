9a8851aabe23d7a36441320a1bf786a1815cc561
youesen <3w.sen.com>
2018-04-29 13:48:25 +0800

    提交温蒂git仓库

---
+++ src/Person.java
@@ -0,0 +1,29 @@
+/**
+ * Description：***<br/>
+ * Copyright (c) ， 2018， Jansonxu <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date：2018年04月20日
+ *
+ * @author 张森
+ * @version : 1.0
+ */
+public class Person {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
